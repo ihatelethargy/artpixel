6ef06c95e3bf9912d759707e7650c7e01e38950e
ihatelethargy <naturalflavor1007@naver.com>
2021-04-04 02:54:58 +0900

    techinfo

--- src/pages/techinfopage/TechInfoPage.js
+++ src/pages/techinfopage/TechInfoPage.js
@@ -1,14 +1,37 @@
-import React from 'react'
-import { Route, Router } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { Route, Router, useHistory } from 'react-router-dom'
 import CustomHeader from '../../components/customheader/CustomHeader'
+import Footer from '../../components/footer/Footer'
 import './TechInfoPage.scss'
+import ContactUs from '../../components/contactus/ContactUs'
 
 function TechInfoPage() {
+  const [title, setTitle] = useState('기술정보')
+  const [menu, setMenu] = useState(['기술정보'])
+  const [menuToggle, setMenuToggle] = useState()
+  const history = useHistory()
+
+  useEffect(() => {
+    console.log(history.location.pathname)
+    if (history.location.pathname === '/community') {
+      setTitle('기술정보')
+      setMenu(['기술정보'])
+    }
+  }, [history.location.pathname])
+
   return (
     <>
-      <div className="techinfopageWrapper">
-        <Route component={CustomHeader} />
+      <Route component={CustomHeader} />
+      <div className="TechInfoPageWrapper">
+        <div className="TechInfoPageImgWrapper"></div>
+
+        <div className="contentsWrapper">
+          <div className="contentsLeft">
+            <ContactUs title={title} menu={menu} />
+          </div>
+        </div>
       </div>
+      <Route component={Footer} />
     </>
   )
 }
